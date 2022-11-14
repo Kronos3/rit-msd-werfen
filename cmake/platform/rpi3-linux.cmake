419c3d5190aa80b3c4852544c11ead38375346dc
Andrei Tumbar <andreitumbar@gmail.com>
2022-11-14 14:40:20 -0500

    Started python switch

--- cmake/platform/rpi3-linux.cmake
+++
@@ -1,8 +0,0 @@
-set(CMAKE_SYSTEM_NAME "raspberry-pi-3")
-
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-# DO NOT EDIT: F prime searches for libs, includes, and packages in the
-# toolchain when cross-compiling.
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
