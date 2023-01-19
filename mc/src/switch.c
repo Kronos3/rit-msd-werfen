25266a55230befdd42b5b54197a2879098b8db7e
Andrei Tumbar <andreitumbar@gmail.com>
2023-01-19 03:32:07 -0500

    Finished motor_step commanding

---
+++ mc/src/switch.c
@@ -0,0 +1,21 @@
+//
+// Created by tumbar on 1/19/23.
+//
+
+#include "switch.h"
+#include "config.h"
+
+GPIO_PinState switch_limit_a_set(void)
+{
+    return HAL_GPIO_ReadPin(LIMIT_A_PORT, LIMIT_A_PIN);
+}
+
+GPIO_PinState switch_limit_b_set(void)
+{
+    return HAL_GPIO_ReadPin(LIMIT_B_PORT, LIMIT_B_PIN);
+}
+
+GPIO_PinState switch_e_stop_set(void)
+{
+    return HAL_GPIO_ReadPin(ESTOP_PORT, ESTOP_PIN);
+}
---
+++ mc/src/switch.h
@@ -0,0 +1,16 @@
+//
+// Created by tumbar on 1/19/23.
+//
+
+#ifndef WERFEN_SWITCH_H
+#define WERFEN_SWITCH_H
+
+#include <stm32f1xx_hal.h>
+
+#include "gbl.h"
+
+GPIO_PinState switch_limit_a_set(void);
+GPIO_PinState switch_limit_b_set(void);
+GPIO_PinState switch_e_stop_set(void);
+
+#endif //WERFEN_SWITCH_H
